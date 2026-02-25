d7b64745d5fd4e107f85fd148a590d29f09305c4
erj-hub <75530556+erj-hub@users.noreply.github.com>
2026-02-25 15:46:10 -0500

    Add files via upload

---
+++ pages/info.py
@@ -0,0 +1,136 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "images/willwood.jpg"
+about_me = "My name is Elijah Jeffres. I am a second year History, Technology, and Society student at Georgia Tech, where I have experience working in museums and archives. I am academically interested in urban landscapes, human rights, art history, and comparative religion. In my (limited) free time, I persue art projects and explore nature. "
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/e-jeffres/"
+my_github_url = "https://github.com/erj-hub"
+my_email_address = "ellajeff1@gmail.com"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in History, Technology, and Society',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': '2028',
+    'GPA': '3.7'
+}
+course_data = {
+    "code":["CS 1301", "CP 4510", "ARCH 4107", "HTS 3018"], 
+    "names":["Intro to CS", "Geographic Info Systems", "Intro to Historic Preservation", "Religions & Cults in US"], 
+    "semester_taken":["4th", "3rd", "2nd", "2nd"],
+    "skills":["Python (kill me)", "Geographic Information Systems (kill me)", "National Register of Historic Places (staring at old buildings)", "Sociology of NRMS (specifically flying spaghetti monsters)"],
+    }
+experience_data = {
+    "Student Assistant at Robert C. Williams Museum of Papermaking": (["- Curated an exhibit for America's 250th anniversary", 
+                                                                          "- Created records for 100+ artifacts in the rare books collection using PastPerfect Museum Software", "- Led educational programming, including papermaking workshops for groups of all ages"], "images/museum.jpg"),
+    "Student Assistant at Georgia Tech Archives":(["- Processed what will be the Georgia Tech Archives’ largest science fiction and author’s collection, of novel drafts, publications, childhood writings, art, and photographs",
+                                                        "- Published finding aids and catalogued collections on ArchivesSpace"], "images/archives.jpg"),
+    "Research Assistant with Georgia Tech School of History and Sociology":(["- Cross-referenced digitized primary sources, including personal letters and record books from the mid-1800s, to identify and describe enslaved individuals and their networks"], "images/hsoc.png")
+
+}
+
+projects_data = {
+    "Passion Project ATL": "Conducted 70 interviews with strangers on the streets of Atlanta, with semi-structured questions about happiness and social issues, to amass a landscape of what individual people are passionate about in their community, created ink illustrations of interviewees, and designed an Instagram WIX website for this project",
+    "CS 1301 Lab 1 Quiz": "I made a silly goofy quiz about my favorite obscure pop culture knowledge"
+}
+
+programming_data = {
+    "Python": 50,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+}
+spoken_icons = {"English": "🇬🇧",
+    "Spanish":"🇪🇸", "Hebrew": "🇮🇱"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "Spanish": "Intermediate",
+    "Hebrew": "Tiny",
+}
+leadership_data = {
+    "LTT Outreachasorous": (["- Improv and rizz (managing communications for a state-wide improv festival, outside shows for)"],"images/improv.jpg"),
+
+}
+activity_data={
+    "LTT": ["- I serve my community by doing improv",]
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+border_picture = "images/tapewormm.png"
+daughter_picture = "images/daughter.jpg"
+eras_picture = "images/eras.jpg"
+world_picture = "images/worldtocome.jpg"
+selfish_picture = "images/selfish.jpg"
+
+
+q1_label = "Who is this?"
+
+q1_options = ["Niel Cicieriga", "Rob Cantor", "Mike Bottiglieri", "Will Wood"]
+
+
+q2_label = "Does Will Wood have a daughter?"
+
+q2_options = ["Yes", "No", "Nobody Knows"]
+
+daughter_explanation = "A few years ago, Will Wood spread a humorous rumor that he had a daughter, even convicing an eleven year old fan to pretend to be his daughter for a photo. He has since dropped the act and confirmed that he does not have a daughter in a 2018 interview."
+
+
+q3_label = '''Will Wood fronted multiple bands before releasing music under his own name. Rank these Will Wood bands in chronological order from when they first emerged:\n
+a. Will Wood and the Tapeworms\n
+b. Strange Thick\n
+c. Jamface\n
+d. A Verbal Equinox\n
+e. The Stereosexuals\n
+f. Will Wood\n'''
+
+
+q4_label = "What city was Will Wood 'slouching towards' in his last tour?"
+
+q4_options = ["Bethlehem","Hell", "Branson", "Mt. Bethel", "Pidgeon Forge"]
+
+
+q5_label = "Which are other names in Will Wood and the Tapeworms' \"Song with Five Names\"? (select 3)"
+
+q5_options = ["Soapbox Tao", "Strange and Profound", "Tree Fall", "Neospace Government", "Checkmate Athiests!"]
+
+
+q6_label = "What symbol is Will Wood known for including in his stage makeup?"
+
+
+q7_label = "What topic is Will Wood's apocalyptic comedy podcast with Chris Dunne, \"Life in the World to Come\", primarily about?"
+
+q7_options = ["Guns", "Babies", "Death", "Mutations"]
+
+
+q8_label = "In what 2 YouTube channels would you be most likely to find obscure will wood content? (select 2)"
+
+q8_options = ["Will Wood Archive", "The Real Will Wood Archive", "Doug Kevendek", "Katy Armstead"]
+
+
